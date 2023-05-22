def4a706780ade316ff49c8bd6d711d35ae79234
Ryan Frawley <me@ryanfrawley.io>
2023-05-22 13:45:29 -0400

    add alacritty and nvim

---
+++ nvim/lua/custom/init.lua
@@ -0,0 +1,19 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+local opt = vim.opt
+opt.relativenumber = true
+
+-- Hides unmodified tab buffers when opening a new one
+vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter", "tabnew" }, {
+  callback = function()
+    vim.t.bufs = vim.tbl_filter(function(bufnr)
+      return vim.api.nvim_buf_get_option(bufnr, "modified")
+    end, vim.t.bufs)
+  end,
+})
