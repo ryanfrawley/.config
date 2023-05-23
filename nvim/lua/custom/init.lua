de5443cd7e4b01bdfa03aa447060db9423de0e99
Ryan Frawley <me@ryanfrawley.io>
2023-05-22 22:31:15 -0400

    remove closing unmodified buffers setting

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -8,12 +8,3 @@
 
 local opt = vim.opt
 opt.relativenumber = true
-
--- Hides unmodified tab buffers when opening a new one
-vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter", "tabnew" }, {
-  callback = function()
-    vim.t.bufs = vim.tbl_filter(function(bufnr)
-      return vim.api.nvim_buf_get_option(bufnr, "modified")
-    end, vim.t.bufs)
-  end,
-})
